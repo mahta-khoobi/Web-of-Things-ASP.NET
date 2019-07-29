4f654bdcdbe9dd08f58083d82d040dc0261dbcf7
Mahta.khoobi <Mahta.khoobi@yahoo.com>
2019-07-29 12:58:58 +0430

    Data classes added

---
+++ IntelligentClassroom/Models/DTO/Actuator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntelligentClassroom.Models.DTO
+{
+    public class Actuator
+    {
+        #region [-ctor-]
+        public Actuator()
+        {
+
+        } 
+        #endregion
+    }
+}
---
+++ IntelligentClassroom/Models/DTO/Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntelligentClassroom.Models.DTO
+{
+    public class Sensor
+    {
+        #region [-ctor-]
+        public Sensor()
+        {
+
+        }
+        #endregion
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Unit { get; set; }
+        public string Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int Frequency { get; set; }
+
+    }
+}
